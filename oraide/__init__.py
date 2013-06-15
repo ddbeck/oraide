fc26853622013c539ae8b81ed901dd240b313e32
Daniel D. Beck <me@danieldbeck.com>
2013-06-14 22:41:15 -0400

    add rewritten oraide package

---
+++ oraide/__init__.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+from contextlib import contextmanager
+import logging
+import random
+import subprocess
+import time
+
+from . import keys as keyboard
+
+
+logger = logging.getLogger('oraide')
+
+
+class ConnectionFailedError(Exception):
+    """The tmux server connection failed (often because the server was not
+    running at the time the command was sent).
+    """
+    def __str__(self):
+        return 'Connection to tmux server failed.'
+
+
+class SessionNotFoundError(Exception):
+    """The tmux session was not found (but a connection to tmux server was
+    established).
+    """
+    def __init__(self, session):
+        self.session = session
+
+    def __str__(self):
+        return 'tmux session {} not found.'.format(repr(self.session))
+
+
+def send_keys(session, keys, literal=True):
+    """Send keys to a tmux session.
+    This function is a wrapper around tmux's ``send-keys`` command.
+
+    If ``literal`` is ``False``, tmux will attempt to convert keynames such as
+    ``Escape`` or ``Space`` to their single-key equivalents.
+
+    :param session: name of a tmux session
+    :param keys: keystrokes to send to the tmux session
+    :param literal: whether to prevent tmux from looking up keynames
+    """
+
+    args = ["tmux", "send-keys"]
+
+    if literal:
+        args.append('-l')
+
+    args.append("-t{}".format(session))
+    args.append(keys)
+
+    logger.info('Sending keys with command: {}'.format(' '.join(args)))
+    try:
+        subprocess.check_output(args, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as exc:
+        if 'session not found' in exc.output:
+            raise SessionNotFoundError(session)
+        elif 'failed to connect to server' in exc.output:
+            raise ConnectionFailedError
+        else:
+            raise
+
+
+def prompt(keys, verb="send"):
+    raw_input("Press enter to {}: {}".format(verb, keys))
+
+
+class Session(object):
+    """A session to which to send keys. This function allows for the
+    deduplication of session names when repeatedly sending keystrokes the same
+    session.
+
+    :param session: the name of a tmux session
+    :param enable_auto_advance: whether to send keystrokes to the session
+        immediately, or wait for confirmation
+    """
+
+    def __init__(self, session, enable_auto_advance=False):
+        self.session = session
+        self.auto_advancing = enable_auto_advance
+
+    def send_keys(self, keys, literal=True, after=None):
+        """Send each literal character in ``keys`` to the session.
+
+        :param keys: literal keystrokes to send to the session
+        :param literal: whether to prevent tmux from looking up keynames
+        """
+        send_keys(self.session, keys, literal=literal)
+
+    def teletype(self, keys, delay=90):
+        """Type ``keys`` character-by-character, as if you were actually typing
+        them by hand.
+
+        The ``delay`` parameter adds time between each keystroke for
+        verisimilitude. The actual time between keystrokes varies up to ten
+        percent more or less than the nominal value. The default, 90
+        milliseconds, approximates a fast typist.
+
+        :param keys: the literal keys to be typed
+        :param int delay: the nominal time between keystrokes in milliseconds.
+        """
+        delay_variation = delay / 10
+
+        with self.auto_advance():
+            for key in keys:
+                self.send_keys(key)
+
+                current_delay = random.randint(delay - delay_variation,
+                                               delay + delay_variation)
+                time.sleep(current_delay / 1000.0)
+
+    def enter(self, keys=None, teletype=True, after=keyboard.enter):
+        """Type ``keys``, then press :kbd:`Enter`.
+
+        By default, typing character-by-character is enabled with the
+        ``teletype`` parameter.
+
+        :param keys: the keystroke to be sent to the to the session. These keys
+            may only be literal keystrokes, not keynames to be looked up by
+            tmux.
+        :param after: additional keystrokes to send to the session with
+            ``literal`` set to ``False`` (typically for appending a special keys
+            from :mod:`oraide.keys`, like the default, :kbd:`Enter`)
+        """
+        if not self.auto_advancing:
+            prompt(keys)
+
+        if teletype:
+            self.teletype(keys)
+        else:
+            self.send_keys(keys)
+
+        with self.auto_advance():
+            self.send_keys(after, literal=False)
+
+    @contextmanager
+    def auto_advance(self):
+        """Return a context manager that disables prompts before sending
+        keystrokes to the session. For example:
+
+        .. code-block:: python
+
+           session.enter('vim some_file.txt')    # prompt first
+           with session.auto_advance():          # disables prompts
+               session.teletype('jjji')
+               session.enter('Hello, World!')
+               session.press_key(keys.escape)
+           session.enter(':x')                   # prompt first
+        """
+        initial_auto_state = self.auto_advancing
+        self.auto_advancing = True
+        yield
+        self.auto_advancing = initial_auto_state
+
+
+__all__ = [send_keys, Session]
---
+++ oraide/keys.py
@@ -0,0 +1,115 @@
+"""This module provides shortcuts for sending special keystrokes to tmux
+sessions. The functions and constants are typically used with
+:meth:`oraide.Session.press_key`, or the ``after`` paramter on certain
+:class:`oraide.Session` methods.
+
+The constants of this module are provided as a Pythonic substitute for the
+strings used in tmux's keyname lookup table. For example, to send a backspace
+key, the string ``'BSpace'`` may be replaced with a reference to
+``oraide.keys.backspace``. The following keys are provided:
+
+* ``backspace``
+* ``end``
+* ``enter``
+* ``escape``
+* ``home``
+* ``page_down``
+* ``page_up``
+* ``space``
+* ``tab``
+* ``up``
+* ``down``
+* ``left``
+* ``right``
+
+as well as the function keys ``f1`` through ``f20``.
+"""
+
+import sys
+
+
+# Function keys (e.g., F1, F2, F3, and so on)
+def _add_f_keys():
+    for i in range(1, 20):
+        setattr(sys.modules[__name__], 'f{}'.format(i), 'F{}'.format(i))
+_add_f_keys()
+
+
+# special keys
+backspace = 'BSpace'
+end = 'End'
+enter = 'Enter'
+escape = 'Escape'
+home = 'Home'
+page_down = 'PageDown'
+page_up = 'PageUp'
+space = 'Space'
+tab = 'Tab'
+
+
+# arrow keys
+up = 'Up'
+down = 'Down'
+left = 'Left'
+right = 'Right'
+
+
+def control(key):
+    """Make a string that tmux will parse as the control key (:kbd:`Ctrl`) and
+    ``key`` pressed at the same time.
+
+    .. note::
+
+       The ``key`` parameter is case-sensitive. If ``key`` is uppercase, it's
+       equivalent to entering :kbd:`Shift` and the key.
+
+       .. doctest::
+
+          >>> from oraide.keys import control
+          >>> control('a')  # ctrl + a
+          'C-a'
+          >>> control('A')  # ctrl + shift + a
+          'C-A'
+       """
+    return 'C-{}'.format(key)
+
+
+def command(key):
+    """Make a string that tmux will parse as the command key (also known as the
+    meta, super, cmd, Apple, and Windows key) and ``key`` pressed at the same
+    time.
+
+    .. note::
+
+       The ``key`` parameter is case-sensitive. If ``key`` is uppercase, it's
+       equivalent to entering :kbd:`Shift` and the key.
+
+       .. doctest::
+
+          >>> from oraide.keys import command
+          >>> command('a')  # command + a
+          'M-a'
+          >>> command('A')  # command + shift + a
+          'M-A'
+    """
+    return 'M-{}'.format(key)
+
+
+def alt(key):
+    """Make a string that tmux will parse as the alt key (:kbd:`Alt`) and
+    ``key`` pressed at the same time.
+
+    .. note::
+
+       The ``key`` parameter is case-sensitive. If ``key`` is uppercase, it's
+       equivalent to entering :kbd:`Shift` and the key.
+
+       .. doctest::
+
+          >>> from oraide.keys import alt
+          >>> alt('a')  # alt + a
+          'A-a'
+          >>> alt('A')  # alt + shift + a
+          'A-A'
+    """
+    return 'A-{}'.format(key)
