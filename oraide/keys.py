d3f50b66351f5cec4795e677425521e1db689c9a
Daniel D. Beck <me@danieldbeck.com>
2013-07-06 16:05:08 -0400

    fix cross-reference to nonexistent method

--- oraide/keys.py
+++ oraide/keys.py
@@ -1,6 +1,6 @@
 """This module provides shortcuts for sending special keystrokes to tmux
 sessions. The functions and constants are typically used with
-:meth:`oraide.Session.press_key`, or the ``after`` parameter on certain
+:meth:`oraide.Session.send_keys`, or the ``after`` parameter on certain
 :class:`oraide.Session` methods.
 
 The constants of this module are provided as a Pythonic substitute for the
