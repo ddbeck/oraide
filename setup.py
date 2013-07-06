c8520689042bc8df98ac48c4e7bb6e2fcbc8dd5e
Daniel D. Beck <me@danieldbeck.com>
2013-07-06 16:13:35 -0400

    add packaging setup.py and manifest

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='oraide',
+    version='0.2.0',
+    author='Daniel D. Beck',
+    author_email='me@danieldbeck.com',
+    description=('A library to help presenters demonstrate terminal sessions '
+                 'hands-free.'),
+    url='https://github.com/ddbeck/oraide',
+    license='BSD',
+    long_description=open('README.rst').read(),
+    packages=['oraide'],
+    test_suite='oraide.tests',
+)
