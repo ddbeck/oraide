83a150285a58f2a290f8ca3aef8043253e7ec160
Daniel D. Beck <me@danieldbeck.com>
2013-08-19 23:58:55 -0400

    stop hand-coding the version number in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
+import oraide
+
 setup(
     name='oraide',
-    version='0.3.0',
+    version=oraide.__version__,
     author='Daniel D. Beck',
     author_email='me@danieldbeck.com',
     description=('A library to help presenters demonstrate terminal sessions '
